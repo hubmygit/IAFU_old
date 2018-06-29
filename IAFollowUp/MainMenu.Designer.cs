103b707e77bea301a5810a78fe3d1e099a96aa82
Haris <haris@testing.gr>
2018-06-29 09:38:37 +0300

    new backgroung image

--- IAFollowUp/MainMenu.Designer.cs
+++ IAFollowUp/MainMenu.Designer.cs
@@ -134,7 +134,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(603, 404);
             this.Controls.Add(this.menuStrip);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
